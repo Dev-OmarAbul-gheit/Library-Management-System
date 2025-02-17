7be8eddd58109a601fc820974549a1c545c6bfbc
Dev, Omar Abul-gheit <dev.omarabulgheit@gmail.com>
2025-02-17 20:39:30 +0200

    Added PasswordResetToken model for password reset functionality

--- system/models.py
+++ system/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.utils import timezone
 
 
 class User(AbstractUser):
@@ -9,3 +10,12 @@ class User(AbstractUser):
 
     def __str__(self) -> str:
         return self.username
+    
+
+class PasswordResetToken(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    token = models.CharField(max_length=5, unique=True)
+    expires_at = models.DateTimeField()
+
+    def __str__(self):
+        return self.token
