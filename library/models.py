97094255eb930e3fc2ef1cc571b805134be7b49e
Dev, Omar Abul-gheit <dev.omarabulgheit@gmail.com>
2025-02-18 17:09:28 +0200

    created <Library> model and run migrations.

--- library/models.py
+++ library/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
-# Create your models here.
+
+class Library(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    location = models.CharField(max_length=255, unique=True)
+    latitude = models.FloatField(validators=[MinValueValidator(-90), MaxValueValidator(90)])
+    longitude = models.FloatField(validators=[MinValueValidator(-180), MaxValueValidator(180)])
+    
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        verbose_name = "Library"
+        verbose_name_plural = "Libraries"
