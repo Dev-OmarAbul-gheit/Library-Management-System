8b69b817e6c2eedb539e0bff6845ef1ffa293b6d
Dev, Omar Abul-gheit <dev.omarabulgheit@gmail.com>
2025-02-18 17:39:17 +0200

    Added <Category> model and registered it in admin panel.

--- library/admin.py
+++ library/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Library, Author
+from .models import Library, Author, Category
 
 @admin.register(Library)
 class LibraryAdmin(admin.ModelAdmin):
@@ -8,4 +8,8 @@ class LibraryAdmin(admin.ModelAdmin):
 
 @admin.register(Author)
 class AuthorAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
     pass
--- library/models.py
+++ library/models.py
@@ -26,3 +26,17 @@ class Author(models.Model):
     class Meta:
         verbose_name = "Author"
         verbose_name_plural = "Authors"
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    description = models.TextField(null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
