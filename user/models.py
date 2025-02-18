d802fade5ffda07f6f742e9821d6f2e00c954754
Dev, Omar Abul-gheit <dev.omarabulgheit@gmail.com>
2025-02-18 17:14:13 +0200

    Added latitude and longitude fields to <User> model with value validation.

--- user/models.py
+++ user/models.py
@@ -1,10 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-from django.utils import timezone
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 
 class User(AbstractUser):
     email = models.EmailField(unique=True)
+    latitude = models.FloatField(validators=[MinValueValidator(-90), MaxValueValidator(90)])
+    longitude = models.FloatField(validators=[MinValueValidator(-180), MaxValueValidator(180)])
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username']
 
