59d00c26db10d82b3d644852071789347bc2ebbd
Dev, Omar Abul-gheit <dev.omarabulgheit@gmail.com>
2025-02-21 16:22:05 +0200

    Enhance BorrowingTransactionSerializer: update validation messages, streamline borrower checks, and adjust fields for clarity.

--- library/serializers.py
+++ library/serializers.py
@@ -32,14 +32,15 @@ class BorrowingTransactionSerializer(serializers.ModelSerializer):
     borrowing_price = serializers.DecimalField(read_only=True, max_digits=6, decimal_places=2)
     class Meta:
         model = BorrowingTransaction
-        fields = ['id', 'book', 'borrower', 'borrowing_price', 'borrowing_date', 'due_date']
+        fields = ['id', 'book', 'borrowing_price', 'borrowing_date', 'due_date']
+        
 
     def validate_book(self, book):
         """
-            Check if the book is already borrowed.
+            Check if the book is being borrowed.
         """
         if book.is_borrowed:
-            raise serializers.ValidationError('The book is already borrowed')
+            raise serializers.ValidationError('The book is now being borrowed, choose another book or try again later.')
         return book
     
     def validate_due_date(self, due_date):
@@ -50,20 +51,24 @@ class BorrowingTransactionSerializer(serializers.ModelSerializer):
             raise serializers.ValidationError(f'Return date cannot be more than {max_period_month} month(s) from today.')
         return due_date
 
-    def validate_borrower(self, borrower):
-        if BorrowingTransaction.objects.filter(borrower=borrower).count() >= 3:
-            raise serializers.ValidationError('The borrower has already reached the maximum number of books allowed to be borrowed (3).')
-        return borrower
-
     def create(self, validated_data):
         book_price = validated_data['book'].book.price
-        borrowing_price = book_price * Decimal(0.1) # assume the borrowing price is 10% of the book price
+        # assume the borrowing price is 10% of the book price
+        borrowing_price = book_price * Decimal(0.1)
         validated_data['borrowing_price'] = borrowing_price
 
+        borrower = self.context['borrower']
+        # check the number of borrowed books by the borrower
+        if BorrowingTransaction.objects.filter(borrower=borrower).count() >= 3:
+            raise serializers.ValidationError('The borrower has already reached the maximum number of books allowed to be borrowed (3).')
+        
+        validated_data['borrower'] = borrower
+        
+        # create the borrowing transaction
         borrowing_transaction = super().create(validated_data)
 
         # set the book as borrowed
         borrowing_transaction.book.is_borrowed = True
-
         borrowing_transaction.book.save()
+        
         return borrowing_transaction
