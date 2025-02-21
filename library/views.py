622046bf2fa8a2617887eddbf2ce64e6d063a579
Dev, Omar Abul-gheit <dev.omarabulgheit@gmail.com>
2025-02-21 16:22:18 +0200

    Update BorrowingTransactionViewSet permissions and add transaction history endpoint

--- library/views.py
+++ library/views.py
@@ -3,7 +3,7 @@ from rest_framework import status
 from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework.viewsets import ReadOnlyModelViewSet, ModelViewSet
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAdminUser, IsAuthenticated
 from django_filters.rest_framework import DjangoFilterBackend
 from .models import Library, Author, Book, BorrowingTransaction
 from .serializers import LibrarySerializer, AuthorSerializer, BookSerializer, BorrowingTransactionSerializer
@@ -37,11 +37,19 @@ class BookViewSet(ReadOnlyModelViewSet):
 class BorrowingTransactionViewSet(ReadOnlyModelViewSet):
     queryset = BorrowingTransaction.objects.all()
     serializer_class = BorrowingTransactionSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAdminUser]
 
-    @action(detail=False, methods=['post'], url_path='borrow', url_name='borrow-book')
+    
+    @action(detail=False, methods=['post'], url_path='borrow', url_name='borrow-book', permission_classes=[IsAuthenticated])
     def borrow_book(self, request):
-        serializer = self.get_serializer(data=request.data)
+        serializer = self.get_serializer(data=request.data, context = {'borrower': request.user})
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
+    
+
+    @action(detail=False, methods=['get'], url_path='history', url_name='transactions-history', permission_classes=[IsAuthenticated])
+    def view_history(self, request):
+        queryset = self.get_queryset().filter(borrower=request.user)
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
