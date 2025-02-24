134c4378a0ec9d0cf9cf7d9eeb678e4f54dae838
Dev, Omar Abul-gheit <dev.omarabulgheit@gmail.com>
2025-02-24 22:36:22 +0200

    Implement asynchronous email sending for OTP in password reset process

--- user_app/mixins.py
+++ user_app/mixins.py
@@ -1,6 +1,7 @@
 from django.core.mail import send_mail
 from django.template.loader import render_to_string
 from django_lifecycle import AFTER_CREATE, LifecycleModelMixin, hook
+from .tasks import send_email_with_otp__async
 
 
 class OTPMixin(LifecycleModelMixin):
@@ -8,14 +9,8 @@ class OTPMixin(LifecycleModelMixin):
     def send_email_with_otp(self):
         data = {
             'username': self.user.username,
+            'email' : self.user.email,
             'otp': self.otp,
             'reset_link' : f'http://127.0.0.1:8000/users/password-reset/confirm/'
         }
-        body = render_to_string('emails/password-reset-email.html', data)
-        send_mail(
-            subject='Password Reset Request',
-            message=body,
-            html_message=body,
-            from_email='admin@platform.com',
-            recipient_list=[self.user.email]
-        )
+        send_email_with_otp__async.delay(data)
