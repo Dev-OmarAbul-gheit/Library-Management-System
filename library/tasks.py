cdf752b7c4ac45dcf158e8db22de9f7b5cbc3fb2
Dev, Omar Abul-gheit <dev.omarabulgheit@gmail.com>
2025-02-25 05:23:08 +0200

    Add asynchronous task for notifying borrowers about upcoming due dates and create email template for reminders

--- library/tasks.py
+++ library/tasks.py
@@ -1,6 +1,8 @@
+from django.utils import timezone
 from django.core.mail import send_mail
 from django.template.loader import render_to_string
 from celery import shared_task
+from .models import BorrowingTransaction
 
 @shared_task
 def send_borrowing_email_async(borrowing_data):
@@ -13,3 +15,33 @@ def send_borrowing_email_async(borrowing_data):
         recipient_list=[borrowing_data['email']],
         html_message=html_message,
     )
+
+@shared_task
+def notify_borrowers_async():
+    queryset = BorrowingTransaction.objects.select_related('borrower').prefetch_related('books__book')
+    transactions = [
+        transaction for transaction in queryset 
+        if transaction.due_date - timezone.now().date() <= timezone.timedelta(days=3) 
+        and transaction.books.filter(is_borrowed=True).exists()
+    ]
+
+    for transaction in transactions:
+        notify_email_data = {
+            'username' : transaction.borrower.username,
+            'books': [{
+                'title': book.book.title,
+                'author': book.book.author.name,
+                'library': book.library.name
+            } for book in transaction.books.filter(is_borrowed=True)],
+            'due_date' : transaction.due_date
+        }
+        html_message = render_to_string('emails/borrowing_reminder_email.html', notify_email_data)
+        send_mail(
+            subject='Borrowing Due Date Reminder',
+            message=html_message,
+            from_email='admin@platform.com',
+            recipient_list=[transaction.borrower.email],
+            html_message=html_message,
+        )
+
+    
