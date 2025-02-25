e3e4fe48631209d54ff68100bed36a27c6df530b
Dev, Omar Abul-gheit <dev.omarabulgheit@gmail.com>
2025-02-25 16:19:31 +0200

    Add throttling settings to REST framework configuration

--- Library_Management_System/settings.py
+++ Library_Management_System/settings.py
@@ -158,8 +158,19 @@ REST_FRAMEWORK = {
         "rest_framework.filters.SearchFilter",
         "rest_framework.filters.OrderingFilter",
     ),
+
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10, 
+    'PAGE_SIZE': 10,
+
+    # Throttling settings
+    'DEFAULT_THROTTLE_CLASSES': [
+        'rest_framework.throttling.AnonRateThrottle',
+        'rest_framework.throttling.UserRateThrottle',
+    ],
+    'DEFAULT_THROTTLE_RATES': {
+        'anon': '100/day',
+        'user': '500/hour',
+    }
 }
 
 from datetime import timedelta
